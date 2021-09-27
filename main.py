2f6fd36a1e3cbcfcee5ea2fd6673bfe478f3994e
kinggkellyy <kfking@loyola.edu>
2021-09-27 12:11:53 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Programmers: [Kelly King and Jomarr Meekins]
+# Course: CS151, Prof. Mehri
+# Date: September 27th, 2021
+# Lab Number:
+# Program Inputs: [What information do you request from the user?]
+# Program Outputs: [What information do you display for the user?]
+
